1b249dfed3d4da82e124c29aa1660a56aa85caee
Henry Gabriel Andrade de Oliveira <henrygabrielandradeoliveira@gmail.com>
2025-11-01 17:07:45 -0300

    Dia 7 de monitoria

---
+++ monitorias/Algoritmos/Praticas/dia7/forIterador.py
@@ -0,0 +1,4 @@
+pessoas = [["joao", 12, 1.65, 100], ["maria", 13, 1.75, 200], ["joze", 15, 1.34, 300]]
+
+for indice_pessoa in range(len(pessoas)):
+    print(f"Nome - {pessoas[indice_pessoa][0]}, Tamanho - {pessoas[indice_pessoa][1]}")
---
+++ monitorias/Algoritmos/Roteiro/dia7.md
@@ -0,0 +1,1 @@
+- Acessar por colchetes, valores
